aeeb0a4bf16d013167b236d484a490ddca04c254
anuvasehgal <asehgal@tcd.ie>
2020-08-28 11:49:03 +0100

    Animal Name change

---
+++ Animal.java
@@ -0,0 +1,23 @@
+class Animal {
+
+    static int weight= 200;
+    static int weight1= weight+ 100;
+    Animal(){
+        System.out.println("Animal in Constructor : " + "Cheetah");
+        System.out.println(weight);
+    }
+
+    String Animal_Name= "Tiger";
+    void Name(){
+        System.out.println("Animal in Function : " + Animal_Name);
+        System.out.println(weight1);
+    }
+
+    public static void main (String[] args) {
+        Animal a= new Animal();
+        for (int i=0; i<2; i++)  a.Name();
+
+
+    }
+
+}
